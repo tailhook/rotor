efaae498b7fffb2b51fb0f81492568f052350441
Paul Colomiets <paul@colomiets.name>
2015-08-08 19:13:12 +0300

    Add initial buffer_util module

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@
 extern crate netbuf;
 extern crate mio;
 #[macro_use] extern crate log;
+extern crate memchr;
 
 pub mod transports;
 pub mod handler;
+pub mod buffer_util;
 
 pub use handler::{EventMachine, Handler};
